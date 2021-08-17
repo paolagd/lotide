68d76a234d06edc4ffb0af943866ed1477ce4256
Paola Garcia <pacri14@gmail.com>
2021-08-17 23:40:57 +0000

    improved assertion function

---
+++ eqArrays.js
@@ -0,0 +1,26 @@
+const eqArrays = (array1, array2) => {
+  for (let i = 0; i < array1.length; i++) { 
+    if (array1[i] !== array2[i]) return false; 
+  }
+  return true;
+}
+
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual([1], [1]);
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1]))// => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
