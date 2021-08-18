566cef4de54871af48fb6b4c2ee508571b7465c1
Paola Garcia <pacri14@gmail.com>
2021-08-18 01:53:40 +0000

    error fix

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -13,14 +13,6 @@ const assertArraysEqual = (array1, array2) => {
   }
 }
 
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-
 // TEST CODE
 //assertEqual("Lighthouse Labs", "Bootcamp");
 //assertEqual([1], [1]);
