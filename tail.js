d7f851bbe04b7560401a3bed32c3f3e4a2d6516d
Paola Garcia <pacri14@gmail.com>
2021-08-23 17:42:53 +0000

    Tail has been refactored as well, assing also tailTest.js

--- tail.js
+++ tail.js
@@ -1,15 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-
-const tail = (array) => {
+const tail = (array) => { 
   return array.slice(1);
 };
 
-const words = ["Yo Yo"];
-console.log(tail(words));
-assertEqual(words.length, 3);
+module.exports = tail;
