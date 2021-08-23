154ff9d2cbe604cdba5924e6f8f5ea85da424d5d
Paola Garcia <pacri14@gmail.com>
2021-08-23 12:50:12 +0000

    Refactored asserEqual and head implementing modules and creating a new test folder for test files

--- head.js
+++ head.js
@@ -1,15 +1,5 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-
 const head = (array) => {
   return array[0];
 }
-
-console.log(head([]));
-assertEqual(head([]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+ 
+module.exports = head;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,5 @@
+const assertEqual = require('../assertEqual');
+
+//Assert Equal during from assertEqual refactor
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual([1], [1]);
