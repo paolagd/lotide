4ee4516f19c5b9ec4e5b9340973855692b310995
Paola Garcia <pacri14@gmail.com>
2021-08-23 18:58:18 +0000

    set project up with Mocha and Chai and converted headTest.js to use that instead

--- middle.js
+++ middle.js
@@ -1,17 +1,3 @@
-const eqArrays = (array1, array2) => {
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) return false;
-  }
-  return true;
-}
-
-const assertArraysEqual = (array1, array2) => {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅✅✅ Assertion Passed: [${array1}] === [${array2}]`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: [${array1}] !== [${array2}]`);
-  }
-}
 
 //returns an array with the middle elements
 const middle = (array) => {
@@ -31,7 +17,4 @@ const middle = (array) => {
   return middleArray;
 }
 
-console.log(assertArraysEqual(middle([1, 2, 3]),[2])) // => [2]
-console.log(middle([1, 2, 3, 4])) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4] 
-
+module.exports = middle; 
